01c0074d1e205e9dcd0c7cf5f40b1babc0565daa
tahany777 <tahanymohammed777@hotmail.com>
2021-08-01 23:24:54 +0300

    new

---
+++ src/component/Footer.js
@@ -0,0 +1,13 @@
+import react from 'react';
+
+class Footer extends react.Component {
+    render(){
+        return(
+            <>
+            <p>Tahany Ali</p>
+            </>
+        );
+    }
+}
+
+export default Footer;
---
+++ src/component/Header.js
@@ -0,0 +1,13 @@
+import react from 'react';
+
+class Header extends react.Component {
+    render(){
+        return(
+            <>
+            <h1>Hello world</h1>
+            </>
+        );
+    }
+}
+
+export default Header;
