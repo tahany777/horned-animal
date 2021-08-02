441c3d062bab342c9fe2aa927d16fc29ecd26c51
tahany777 <tahanymohammed777@hotmail.com>
2021-08-02 21:26:58 +0300

    props and state and bootstrap

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import Header from './component/Header';
 import Main from './component/Main';
 import Footer from './component/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
 
 class App extends Component {
   render(){
     return(
-      <>
-      <Header />
-      <Main />
-      <Footer />
-      </>
+      <div className="container container-sm container-md container-lg container-xl">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
     );
   }
 }
--- src/component/HornedBeast.js
+++ src/component/HornedBeast.js
@@ -1,13 +1,29 @@
 import react from 'react';
+import heart from '../img/heart.png';
+
 class HornedBeast extends react.Component{
-    render(){
-        return(
-            <div>
-                <h2>{this.props.title}</h2>
-                <img src={this.props.imageUrl} alt=""/>
-                <p>{this.props.description}</p>
-            </div>  
-        )   
-    }
+  constructor(){
+    super();
+    this.state = {
+      favorites: 0
+    };
+  }
+  addFavorite = (favorites) => {
+    let fav = 1 + favorites;
+    this.setState({
+      favorites: fav
+    });
+  }
+  render(){
+    return(
+      <div className='card flex align-items-center m-2' style ={{width:'20rem', height: '30rem'}}>
+        <h4 className='card-header'>{this.props.title}</h4>
+        <img style ={{maxWidth:'100%', height:'300px', cursor: 'pointer'}} onClick={() => this.addFavorite(this.state.favorites)} src={this.props.imageUrl} alt=""/>
+        <p className='card-text text-center p-2'>{this.props.description}</p>
+        <img style={{width: '20px',height: '20px'}} src={heart} alt=''/>
+        <p>{this.state.favorites}</p>
+      </div>
+    );
+  }
 }
 export default HornedBeast;
