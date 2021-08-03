6a2aff899046c657926b112319ce1ca55543ddf4
tahany777 <tahanymohammed777@hotmail.com>
2021-08-03 19:51:32 +0300

    new edit

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,20 +1,34 @@
-import react from 'react';
+import React, { Component } from 'react';
 import HornedBeast from './HornedBeast';
 import data from '../json/data.json';
 
-class Main extends react.Component {
+export class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      show: 'flex',
+    };
+  }
+  handleSearch = () => {
+    this.setState({
+      show: 'none',
+    });
+  };
   render(){
     return(
-      <main className='d-flex flex-row justify-content-around align-items-center flex-wrap p-2'>
-        {data.map(animal =>
-          <HornedBeast
-            key={animal.title}
-            title={animal.title}
-            imageUrl={animal.image_url}
-            description={animal.description}
-          />
-        )}
-      </main>
+      <div>
+        <input type='text' onKeyUp={this.handleSearch} />
+        <main className='d-flex flex-row justify-content-around align-items-center flex-wrap p-2'>
+          {data.map(animal =>
+            <HornedBeast
+              key={animal.title}
+              title={animal.title}
+              imageUrl={animal.image_url}
+              description={animal.description}
+            />
+          )}
+        </main>
+      </div>
     );
   }
 }
